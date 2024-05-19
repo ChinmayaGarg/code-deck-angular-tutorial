e06ea2a7932543fb633d8e9498ae8bec1c42e281
Chinmaya Garg <ch745692@dal.ca>
2024-05-19 23:16:34 +0530

    getProducts in product service and product param types added

--- tutorial-code-deck/src/types.ts
+++ tutorial-code-deck/src/types.ts
@@ -26,3 +26,29 @@ export interface Options {
       }
     | boolean;
 }
+
+export interface Products {
+  items: Product[];
+  total: number;
+  page: number;
+  perPage: number;
+  totalPages: number;
+}
+
+export interface Product {
+  id?: number;
+  price: string;
+  name: string;
+  image: string;
+  rating: number;
+}
+
+export interface PaginationParams {
+  [param: string]:
+    | string
+    | number
+    | boolean
+    | ReadonlyArray<string | number | boolean>;
+  page: number;
+  perPage: number;
+}
