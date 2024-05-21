af5ea317638ebcf4207f1a2da609afc85979d800
Chinmaya Garg <ch745692@dal.ca>
2024-05-21 20:18:03 +0530

    ngOnInit used to initialize Home component
    subscribed to ProductService and getProducts Observable.

--- tutorial-code-deck/src/app/home/home.component.ts
+++ tutorial-code-deck/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home', // name of the component, this is the name we use to call our home component Eg: <app-home></app-home>
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html', // html file
   styleUrl: './home.component.scss', // style file for this component only
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private productService: ProductsService) {}
+
+  // getProducts returns Observable and then we are subscribing to that Observable using ".subscribe". Once that subscription goes through we are logging the products.
+  ngOnInit() {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products);
+      });
+  }
+}
